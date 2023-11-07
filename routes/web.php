d155b31caab1c623a0a793420445407fe416c877
AzeemUlRehman71 <azeemulrehman71@gmail.com>
2023-11-07 15:21:11 +0500

    product detail slider issue

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/cache', function() {
 
-    Artisan::call('cache:clear');
-    Artisan::call('route:clear');
-    Artisan::call('view:clear');
-    Artisan::call('config:clear');
     Artisan::call('config:cache');
     return 'Cache cleared';
 });
